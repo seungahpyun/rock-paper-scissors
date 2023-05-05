d89c26cf39313e0c8ec7ef067851ee0f68e13899
seungahpyun <seungahpyun@icloud.com>
2023-05-05 15:31:54 +0200

    add if statement

--- javascript.js
+++ javascript.js
@@ -1,9 +1,54 @@
-// const userChoice = prompt("Rock-Paper-Scissors?");
+let playerScore = 0
+let computerScore = 0
 
-function getComputerChoice() {
-  let RockPaperScissors = ['Rock', 'Paper', 'Scissors'];
-  let random = Math.floor(Math.random() * RockPaperScissors.length);
-  return RockPaperScissors[random]
+function playerSelection() {
+  let selection = (prompt("Please Enter Rock, Paper or Scissors")).toLocaleLowerCase;
+  return selection
 }
 
-console.log(getComputerChoice())
+
+function getRandomChoice() {
+  let randomNumber = Math.floor(Math.random() * 3)
+  switch (randomNumber) {
+    case 0:
+      return 'rock'
+    case 1:
+      return 'paper'
+    case 2:
+      return 'scissors'
+  }
+}
+
+let computerSelection = getRandomChoice();
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    roundWinner = 'tie'
+  }
+  if (
+    (playerSelection === 'rock' && computerSelection === 'scissors') ||
+    (playerSelection === 'scissors' && computerSelection === 'paper') ||
+    (playerSelection === 'paper' && computerSelection === 'rock')
+  ) {
+    playerScore++
+    roundWinner = 'player'
+  }
+  if (
+    (computerSelection === 'rock' && playerSelection === 'scissors') ||
+    (computerSelection === 'scissors' && playerSelection === 'paper') ||
+    (computerSelection === 'paper' && playerSelection === 'rock')
+  ) {
+    computerScore++
+    roundWinner = 'computer'
+  }
+  // updateScoreMessage(roundWinner, playerSelection, computerSelection)
+}
+
+
+
+function isGameOver() {
+  return playerScore === 5 || computerScore === 5
+}
+
+
+console.log(playRound(playerSelection(), computerSelection));
